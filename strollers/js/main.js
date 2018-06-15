c9ba1a6ca2eff4c5a19b51ab5a64ca2840e24098
Andrew <bezhanovkh@gmail.com>
2018-06-15 22:09:51 +0300

    2
    3

---
+++ strollers/js/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+
+$('.video_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
