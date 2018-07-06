9e3cf8210ede60cd6501fbe76535504dd6882ce5
Andrew <bezhanovkh@gmail.com>
2018-07-06 12:07:39 +0300

    qwe
    qwe

--- just/js/main.js
+++ just/js/main.js
@@ -16,4 +16,22 @@ $('.slider').slick({
     cssEase: 'linear'
 });
 
+// input validation
 
+$(document).ready(function() {
+    $('#email').blur(function() {
+        if($(this).val() != '') {
+            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if(pattern.test($(this).val())){
+                $(this).css({'border' : '1px solid #b5a263'});
+                $('#valid').text('');
+            } else {
+                $(this).css({'border' : '1px solid #ff0000'});
+                $('#valid').text('Invalid address');
+            }
+        } else {
+            $(this).css({'border' : '1px solid #ff0000'});
+            $('#valid').text('The email field must not be empty');
+        }
+    });
+});
